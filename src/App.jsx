a9b9723b36f8814392bcfae2abbf31b001c14801
Tiana-K08 <tetkononenko2020@gmail.com>
2025-10-27 12:37:56 +0200

    feat: destructuring object in props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ParentGreeting from './components/props/data/ParentGreeting.jsx';
 import ParentProfile from './components/props/object/ParentProfile.jsx';
+import ParentProfileDest from './components/props/destructuring/ParentProfileDest.jsx';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="app-wrapper">
       <ParentGreeting />
       <ParentProfile />
+      <ParentProfileDest />
     </div>
   );
 }
