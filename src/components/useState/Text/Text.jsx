9a1ae9f2b56a81b1d8a8eff6ac285bee119296b0
Tiana-K08 <tetkononenko2020@gmail.com>
2025-10-29 10:18:23 +0200

    refactor: generate buttons text color dynamically via .map()

--- src/components/useState/Text/Text.jsx
+++ src/components/useState/Text/Text.jsx
@@ -12,21 +12,11 @@ function Text() {
 
   const [textColor, setTextColor] = useState(initialColor);
 
-  const handleTextRed = () => {
-    setTextColor(redColor);
-  };
-
-  const handleTextBlue = () => {
-    setTextColor(blueColor);
-  };
-
-  const handleTextGreen = () => {
-    setTextColor(greenColor);
-  };
-
-  const handleResetColor = () => {
-    setTextColor(initialColor);
-  };
+  const buttons = [
+    { color: redColor, label: 'Text Red' },
+    { color: blueColor, label: 'Text Blue' },
+    { color: greenColor, label: 'Text Green' },
+  ];
 
   return (
     <div className="text-wrapper">
@@ -40,29 +30,22 @@ function Text() {
         rerum, saepe velit ex.
       </p>
       <div className="btn-container">
-        <Button
-          className="styled-button"
-          onClick={handleTextRed}
-          label="Text Red"
-          activeColor={textColor === redColor ? redColor : initialColor}
-        />
-        <Button
-          className="styled-button"
-          onClick={handleTextBlue}
-          label="Text Blue"
-          activeColor={textColor === blueColor ? blueColor : initialColor}
-        />
-        <Button
-          className="styled-button"
-          onClick={handleTextGreen}
-          label="Text Green"
-          activeColor={textColor === greenColor ? greenColor : initialColor}
-        />
+        {buttons.map((button) => (
+          <Button
+            key={button.color}
+            className="styled-button"
+            onClick={() => setTextColor(button.color)}
+            label={button.label}
+            activeColor={
+              textColor === button.color ? button.color : initialColor
+            }
+          />
+        ))}
       </div>
       <div className="btn-container">
         <Button
           className="styled-button"
-          onClick={handleResetColor}
+          onClick={() => setTextColor(initialColor)}
           label="Reset Color"
         />
       </div>
